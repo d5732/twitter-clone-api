73cabfe3cf2a190c9cf45359a2a5d396811ef38c
Allen Green <allenngreen@gmail.com>
2023-01-03 11:26:53 -0600

    Completed 3 good first starts tasks for assessment

---
+++ src/main/java/com/cooksys/team2assessment/Team2assessmentApplication.java
@@ -0,0 +1,13 @@
+package com.cooksys.team2assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team2assessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team2assessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cooksys/team2assessment/Team2assessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooksys.team2assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team2assessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
