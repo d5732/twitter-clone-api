2f6c5bb9f8c4ccb380a0d7460fc66b6de110c7ec
Helena Makendengue <HelenaMakens1013@gmail.com>
2023-01-04 11:29:19 -0600

    Update Credentials.java

--- src/main/java/com/cooksys/twitter_api/entities/Credentials.java
+++ src/main/java/com/cooksys/twitter_api/entities/Credentials.java
@@ -1,8 +1,13 @@
 package com.cooksys.twitter_api.entities;
 
-import jakarta.persistence.Embeddable;
+import javax.persistence.Embeddable;
+
+import lombok.NoArgsConstructor;
+import lombok.Data;
 
 @Embeddable
+@NoArgsConstructor
+@Data
 public class Credentials {
 
     private String username;
