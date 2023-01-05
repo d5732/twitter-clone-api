859d45014aedc9f2582ea6f066addeb2e7207703
Allen Green <allenngreen@gmail.com>
2023-01-04 22:36:04 -0600

    Add lombok path to pom.xml and removed comment markings from correct returns on TwitterControllerAdvice

--- src/main/java/com/cooksys/twitter_api/controllers/advice/TwitterControllerAdvice.java
+++ src/main/java/com/cooksys/twitter_api/controllers/advice/TwitterControllerAdvice.java
@@ -13,28 +13,24 @@ import com.cooksys.twitter_api.exceptions.NotAuthorizedException;
 
 import jakarta.servlet.http.HttpServletRequest;
 
-@ControllerAdvice(basePackages = {"com.cooksys.twitter_api"})
+@ControllerAdvice(basePackages = { "com.cooksys.twitter_api.controllers" })
 @ResponseBody
 public class TwitterControllerAdvice {
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(BadRequestException.class)
-    public ErrorDto handleBadRequestException(HttpServletRequest request, BadRequestException badRequestException) {
-//		return new ErrorDto(badRequestException.getMessage());
-        //TODO: negotiate these errors
-        return null;
-    }
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(BadRequestException.class)
+	public ErrorDto handleBadRequestException(HttpServletRequest request, BadRequestException badRequestException) {
+		return new ErrorDto(badRequestException.getMessage());
+	}
 
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler(NotFoundException.class)
-    public ErrorDto handleNotFoundException(HttpServletRequest request, NotFoundException notFoundException) {
-//		return new ErrorDto(notFoundException.getMessage());
-        return null;
-    }
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(NotFoundException.class)
+	public ErrorDto handleNotFoundException(HttpServletRequest request, NotFoundException notFoundException) {
+		return new ErrorDto(notFoundException.getMessage());
+	}
 
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    @ExceptionHandler(NotAuthorizedException.class)
-    public ErrorDto handleNotFoundException(HttpServletRequest request, NotAuthorizedException notAuthorizedException) {
-//		return new ErrorDto(notAuthorizedException.getMessage());
-        return null;
-    }
+	@ResponseStatus(HttpStatus.UNAUTHORIZED)
+	@ExceptionHandler(NotAuthorizedException.class)
+	public ErrorDto handleNotFoundException(HttpServletRequest request, NotAuthorizedException notAuthorizedException) {
+		return new ErrorDto(notAuthorizedException.getMessage());
+	}
 }
