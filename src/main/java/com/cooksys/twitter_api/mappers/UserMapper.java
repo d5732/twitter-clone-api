1ebcb508e1f93044336a9cd746e656358cded8dd
d5732 <denwa.dev@gmail.com>
2023-01-05 03:00:18 -0600

    feat: UserMapper

--- src/main/java/com/cooksys/twitter_api/mappers/UserMapper.java
+++ src/main/java/com/cooksys/twitter_api/mappers/UserMapper.java
@@ -1,11 +1,9 @@
 package com.cooksys.twitter_api.mappers;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-
 import com.cooksys.twitter_api.dtos.UserRequestDto;
 import com.cooksys.twitter_api.dtos.UserResponseDto;
 import com.cooksys.twitter_api.entities.User;
+import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring", uses = { ProfileMapper.class, CredentialsMapper.class })
 public interface UserMapper {
@@ -14,6 +12,6 @@ public interface UserMapper {
 //	@Mapping(target = "username", source = "credentials.username")
 	UserResponseDto entityToDto(User user);
 
-	User DtoToEntity(UserRequestDto userRequestDto);
+	User dtoToEntity(UserRequestDto userRequestDto);
 
 }
