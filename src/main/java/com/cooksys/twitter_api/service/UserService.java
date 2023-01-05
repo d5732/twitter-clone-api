4d45b83366cb8113f5a5215d8f6d5ca49de43499
d5732 <denwa.dev@gmail.com>
2023-01-05 02:57:16 -0600

    feat: UserServiceImpl

--- src/main/java/com/cooksys/twitter_api/service/UserService.java
+++ src/main/java/com/cooksys/twitter_api/service/UserService.java
@@ -1,5 +1,83 @@
 package com.cooksys.twitter_api.service;
 
+import com.cooksys.twitter_api.dtos.CredentialsDto;
+import com.cooksys.twitter_api.dtos.ProfileDto;
+import com.cooksys.twitter_api.dtos.TweetResponseDto;
+import com.cooksys.twitter_api.dtos.UserResponseDto;
+
+import java.util.List;
+
 public interface UserService {
 
+    /**
+     * GET users/@{username}
+     * #42
+     */
+    UserResponseDto getUser(String username);
+
+    /**
+     * PATCH users/@{username}
+     * #41
+     */
+    UserResponseDto updateUserProfile(String username, CredentialsDto credentialsDto, ProfileDto profileDto);
+
+    /**
+     * DELETE users/@{username}
+     * #40
+     */
+    UserResponseDto deleteUser(String username, CredentialsDto credentialsDto);
+
+    /**
+     * POST users/@{username}/follow
+     * #39
+     */
+    void followUser(String username, CredentialsDto credentialsDto);
+
+    /**
+     * POST users/@{username}/unfollow
+     * #38
+     */
+    void unfollowUser(String username, CredentialsDto credentialsDto);
+
+    /**
+     * GET users/@{username}/feed
+     * #67
+     */
+    List<TweetResponseDto> getFeed(String username);
+
+    /**
+     * GET users/@{username}/tweets
+     * #66
+     */
+    List<TweetResponseDto> getTweets(String username);
+
+    /**
+     * GET users/@{username}/mentions
+     * #65
+     */
+    List<TweetResponseDto> getMentions(String username);
+
+    /**
+     * GET users/@{username}/followers
+     * #64
+     */
+    List<UserResponseDto> getFollowers(String username);
+
+    /**
+     * GET users/@{username}/following
+     * #63
+     */
+    List<UserResponseDto> getFollowing(String username);
+
+    /**
+     * GET users
+     * #44
+     */
+    List<UserResponseDto> getAllUsers();
+
+    /**
+     * POST users
+     * #43
+     */
+    UserResponseDto createUser(CredentialsDto credentialsDto, ProfileDto profileDto);
 }
