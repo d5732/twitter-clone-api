1ffb4d0fad5463baa6d48f659c99f69cadda7648
d5732 <denwa.dev@gmail.com>
2023-01-05 03:04:47 -0600

    fix: TweetRepository Annot., Timestamp, camelCase
    fix: java.sql.Timestamp
    
    fix: camelCase
    
    fix: camelCase

--- src/main/java/com/cooksys/twitter_api/mappers/HashtagMapper.java
+++ src/main/java/com/cooksys/twitter_api/mappers/HashtagMapper.java
@@ -7,8 +7,7 @@ import com.cooksys.twitter_api.entities.Hashtag;
 
 @Mapper(componentModel = "spring")
 public interface HashtagMapper {
-	HashtagDto entityToDto(Hashtag hashtag);
+    HashtagDto entityToDto(Hashtag hashtag);
 
-	Hashtag DtoToEntity(HashtagDto hashtagDto);
-
+    Hashtag dtoToEntity(HashtagDto hashtagDto);
 }
--- src/main/java/com/cooksys/twitter_api/repositories/TweetRepository.java
+++ src/main/java/com/cooksys/twitter_api/repositories/TweetRepository.java
@@ -1,16 +1,17 @@
 package com.cooksys.twitter_api.repositories;
 
+import com.cooksys.twitter_api.entities.Tweet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cooksys.twitter_api.entities.Tweet;
-
+@Repository
 public interface TweetRepository extends JpaRepository<Tweet, Long> {
 
-	Optional<Tweet> findByIdAndDeletedFalse(Long id);
+    Optional<Tweet> findByIdAndDeletedFalse(Long id);
 
-	List<Tweet> findAllByDeletedFalse();
+    List<Tweet> findAllByDeletedFalse();
 
 }
