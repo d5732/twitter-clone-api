a0a8e622233838a2ae18ce65ec37e715e10da16a
d5732 <denwa.dev@gmail.com>
2023-01-06 07:20:42 -0600

    WIP - user_mentions join table for almost working

--- src/main/java/com/cooksys/twitter_api/repositories/HashtagRepository.java
+++ src/main/java/com/cooksys/twitter_api/repositories/HashtagRepository.java
@@ -1,16 +1,16 @@
 package com.cooksys.twitter_api.repositories;
 
-import java.util.List;
-import java.util.Optional;
-
+import com.cooksys.twitter_api.entities.Hashtag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.cooksys.twitter_api.entities.Hashtag;
-import com.cooksys.twitter_api.entities.Tweet;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
+
+	Optional<Hashtag> findByLabelAndDeletedFalse(String label);
 
 	Optional<Hashtag> findByIdAndDeletedFalse(Long id);
 
