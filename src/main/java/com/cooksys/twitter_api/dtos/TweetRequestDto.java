ad0840aab1fc32c3af7c8b2927aae01738b6c8b0
d5732 <denwa.dev@gmail.com>
2023-01-06 03:03:40 -0600

    fix: TweetRequestDto

--- src/main/java/com/cooksys/twitter_api/dtos/TweetRequestDto.java
+++ src/main/java/com/cooksys/twitter_api/dtos/TweetRequestDto.java
@@ -3,14 +3,12 @@ package com.cooksys.twitter_api.dtos;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @NoArgsConstructor
 @Data
 public class TweetRequestDto {
 
 	private String content;
 
-	List<CredentialsDto> credentials;
+	CredentialsDto credentials;
 
 }
